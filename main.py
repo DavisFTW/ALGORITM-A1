abaf300c179e7de1ae030c83f57270b5e3063000
DavisFTW <31802124+DavisFTW@users.noreply.github.com>
2021-06-16 15:09:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import ctypes
 
-def split(string: str) -> ctypes.Array:
-    return [char for char in str]
+def split( string: str ) -> ctypes.Array:
+    return [char for char in string]
 
-def list_to_string(array) -> str:
+def list_to_string( array ) -> str:
     str = ""
     for i in array:
         str += i
     return str
 
-def sort_alphabetical_order(string: str) -> str:
+def sort_alphabetical_order(string : str) -> str:
+
+    string = string.lower() # FIXME: without this we will get wrong results as capital chars have a lower number then lowercase
     array = split(string)
-    array_sorted = sorted(array)
-    new_string = list_to_string(array_sorted)
+
+    n = len(array)
+
+    for i in range(n):
+        for j in range(n - i - 1):
+            if array[j] > array[j + 1]:
+                temp_var = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp_var
+                
+    
+    new_string = list_to_string(array)
     return new_string
 
 str = input(" input any text you would like to sort in alphabetical order! ")
-new_str = sort_alphabetical_order(str)
-print(new_str)
+
+
+new_string = sort_alphabetical_order(str)
+print(new_string)
